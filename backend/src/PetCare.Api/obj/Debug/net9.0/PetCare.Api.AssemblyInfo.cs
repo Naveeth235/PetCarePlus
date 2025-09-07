92737258fdaa5e12b68eb5764e47ee31e4902f96
Kanchan1015 <akanchan0315@gmail.com>
2025-09-07 21:19:28 +0530

    branch for ui

--- backend/src/PetCare.Api/obj/Debug/net9.0/PetCare.Api.AssemblyInfo.cs
+++ backend/src/PetCare.Api/obj/Debug/net9.0/PetCare.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetCare.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+941540dd0363a9c0db1dbc25a84de8b33b87ce14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cab5905a3387a464c5666366606fc0d4207f1f5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetCare.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetCare.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
